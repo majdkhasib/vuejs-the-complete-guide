18c54adab3b6e2d745db14579a2537cd27dbe602
Majd Khasib <majdkhasib@cloud.upwork.com>
2022-03-30 18:47:51 +0300

    Fix:Fixing reset battelogs bug

--- 4-Coursethe-monster/prj-monster-01-starting-setup/app.js
+++ 4-Coursethe-monster/prj-monster-01-starting-setup/app.js
@@ -118,7 +118,7 @@ const app = Vue.createApp({
       this.winner = null;
       this.playerHealth = 100;
       this.monsterHealth = 100;
-      this.logs = [];
+      this.battleLogs = [];
       console.clear();
     },
   },
