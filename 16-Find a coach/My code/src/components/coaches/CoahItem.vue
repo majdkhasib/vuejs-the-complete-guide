a2db72a75c259a31ec54c9c2f03f816f83a154c7
Majd Khasib <majdkhasib@cloud.upwork.com>
2022-04-07 14:06:16 +0300

    Feat:Adding CoachItem component with simple styles

---
+++ 16-Find a coach/My code/src/components/coaches/CoahItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <li>
+    <h3>{{ fullName }}</h3>
+    <h4>${{ rate }}/hour</h4>
+    <div>
+      <span v-for="area in areas" :key="area">{{ area }}</span>
+    </div>
+    <div class="actions">
+      <router-link :to="coachContactLink">Contact</router-link>
+      <router-link :to="coachDeailsLink">View Details</router-link>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['id', 'firstName', 'lastName', 'rate', 'areas'],
+  computed: {
+    fullName() {
+      return this.firstName + ' ' + this.lastName;
+    },
+    coachContactLink() {
+      return this.$route.path + '/' + this.id + '/contact';
+    },
+    coachDeailsLink() {
+      return this.$route.path + '/' + this.id;
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  border: 1px solid black;
+  padding: 1em;
+  margin: 1em;
+}
+.actions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- 16-Find a coach/My code/src/pages/coaches/CoachesList.vue
+++ 16-Find a coach/My code/src/pages/coaches/CoachesList.vue
@@ -2,22 +2,38 @@
   <div>
     <section>FILTER</section>
     <section>
-      <ul>
-        <li v-for="coach in coaches" :key="coach.id">{{ coach.firstName }}</li>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in coaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+          >{{ coach.firstName }}</coach-item
+        >
       </ul>
+      <h3 v-else>No coaches found</h3>
     </section>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+import CoachItem from '../../components/coaches/CoahItem.vue';
 export default {
+  components: { CoachItem },
   computed: {
-    coaches() {
-      return this.$store.getters['coaches/coaches'];
-    },
+    ...mapGetters('coaches', ['coaches', 'hasCoaches']),
   },
 };
 </script>
 
-<style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
 </style>
