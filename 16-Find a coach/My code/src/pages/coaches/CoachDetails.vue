fb19d4b4ce4a6e8a07ee9fa31e225be8e8b09cfb
Majd Khasib <majdkhasib@cloud.upwork.com>
2022-04-07 14:06:16 +0300

    Feat:adding coach details

--- 16-Find a coach/My code/src/pages/coaches/CoachDetails.vue
+++ 16-Find a coach/My code/src/pages/coaches/CoachDetails.vue
@@ -1,13 +1,56 @@
 <template>
-  <h2>Coach with id({{ id }})</h2>
+  <section>
+    <base-card
+      ><h2>{{ fullName }}</h2>
+      <h3>${{ rate }}/hour</h3></base-card
+    >
+  </section>
+  <section>
+    <base-card
+      ><h2>Interrested?Reach out</h2>
+      <base-button link :to="contactLink">Contact</base-button></base-card
+    >
+  </section>
+  <section>
+    <base-card
+      ><base-badge
+        v-for="area in areas"
+        :key="area"
+        :type="area"
+        :title="area"
+      ></base-badge>
+      <p>{{ description }}</p>
+    </base-card>
+  </section>
 </template>
 
 <script>
 export default {
+  props: ['id'],
+  state() {
+    return { currentCoach: null };
+  },
   computed: {
-    id() {
-      return this.$route.params.id;
+    fullName() {
+      return this.currentCoach.firstName + ' ' + this.currentCoach.lastName;
     },
+    rate() {
+      return this.currentCoach.hourlyRate;
+    },
+    contactLink() {
+      return this.$route.path + '/' + this.id + '/contact';
+    },
+    areas() {
+      return this.currentCoach.areas;
+    },
+    description() {
+      return this.currentCoach.description;
+    },
+  },
+  created() {
+    console.log(this.id);
+    const coaches = this.$store.getters['coaches/coaches'];
+    this.currentCoach = coaches.find((coach) => coach.id === this.id);
   },
 };
 </script>
