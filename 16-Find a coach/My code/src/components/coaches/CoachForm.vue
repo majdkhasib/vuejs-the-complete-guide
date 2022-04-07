9cbaaa1dd0f473923661afbd7c1df9e8ee68840a
Majd Khasib <majdkhasib@cloud.upwork.com>
2022-04-07 14:06:16 +0300

    Feat:Adding registeration form with styling

---
+++ 16-Find a coach/My code/src/components/coaches/CoachForm.vue
@@ -0,0 +1,96 @@
+<template>
+  <form @submit.prevent="submit">
+    <div class="form-control">
+      <label for="firstname">Firstname</label>
+      <input type="text" id="firstname" v-model.trim="firstName" />
+    </div>
+    <div class="form-control">
+      <label for="lastname">Lastname</label>
+      <input type="text" id="lastname" v-model.trim="lastName" />
+    </div>
+    <div class="form-control">
+      <label for="description">Description</label>
+      <textarea id="description" rows="5" v-model.trim="description"></textarea>
+    </div>
+    <div class="form-control">
+      <label for="rate">Hourly Rate</label>
+      <input type="number" id="rate" v-model.number="rate" />
+    </div>
+    <div class="form-control">
+      <h3>Areas of Expertise</h3>
+      <div>
+        <input type="checkbox" id="frontend" value="frontend" v-model="areas" />
+        <label for="frontend" class="checkbox-label"
+          >Frontend Development</label
+        >
+      </div>
+      <div>
+        <input type="checkbox" id="backend" value="backend" v-model="areas" />
+        <label for="backend" class="checkbox-label">Backend Development</label>
+      </div>
+      <div>
+        <input type="checkbox" id="career" value="career" v-model="areas" />
+        <label for="career" class="checkbox-label">Career Advisory</label>
+      </div>
+    </div>
+    <base-button>Register</base-button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      description: '',
+      rate: null,
+      areas: [],
+    };
+  },
+  methods: {
+    submit() {},
+  },
+};
+</script>
+
+<style>
+.form-control {
+  margin: 1em 0;
+}
+
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5em;
+}
+
+.checkbox-label {
+  font-weight: normal;
+  display: inline;
+  margin: 0 0 0 0.5rem;
+}
+
+input,
+textarea {
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+}
+
+input:focus,
+textarea:focus {
+  background-color: #f0e6fd;
+  outline: none;
+  border-color: #3d008d;
+}
+
+input[type='checkbox'] {
+  display: inline;
+  width: auto;
+}
+
+input[type='checkbox']:focus {
+  outline: #3d008d solid 1px;
+}
+</style>
--- 16-Find a coach/My code/src/pages/coaches/CoachRegisteration.vue
+++ 16-Find a coach/My code/src/pages/coaches/CoachRegisteration.vue
@@ -1,7 +1,15 @@
-<template><h1>Register</h1></template>
+<template>
+  <section>
+    <base-card
+      ><h2>Register as a coach now!</h2>
+      <coach-form></coach-form
+    ></base-card>
+  </section>
+</template>
 
 <script>
-export default {};
+import CoachForm from '../../components/coaches/CoachForm';
+export default { components: { CoachForm } };
 </script>
 
 <style>
