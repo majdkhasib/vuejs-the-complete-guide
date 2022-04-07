c2749ae90718a66f6d6542fe90879f4bfaa9baf1
Majd Khasib <majdkhasib@cloud.upwork.com>
2022-04-07 14:06:16 +0300

    Feat: Adding Base UI components

--- 16-Find a coach/My code/src/components/coaches/CoahItem.vue
+++ 16-Find a coach/My code/src/components/coaches/CoahItem.vue
@@ -3,11 +3,19 @@
     <h3>{{ fullName }}</h3>
     <h4>${{ rate }}/hour</h4>
     <div>
-      <span v-for="area in areas" :key="area">{{ area }}</span>
+      <base-badge
+        v-for="area in areas"
+        :key="area"
+        :type="area"
+        :title="area"
+        >{{ area }}</base-badge
+      >
     </div>
     <div class="actions">
-      <router-link :to="coachContactLink">Contact</router-link>
-      <router-link :to="coachDeailsLink">View Details</router-link>
+      <base-button mode="outline" link :to="coachContactLink"
+        >Contact</base-button
+      >
+      <base-button link :to="coachDeailsLink">View Details</base-button>
     </div>
   </li>
 </template>
@@ -31,12 +39,16 @@ export default {
 
 <style scoped>
 li {
-  border: 1px solid black;
+  border: 1px solid #424242;
+  border-radius: 12px;
   padding: 1em;
   margin: 1em;
 }
 .actions {
   display: flex;
   justify-content: flex-end;
+}
+div {
+  margin: 0.5rem 0;
 }
 </style>
--- 16-Find a coach/My code/src/pages/coaches/CoachesList.vue
+++ 16-Find a coach/My code/src/pages/coaches/CoachesList.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <section>FILTER</section>
-    <section>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register as Coach</base-button>
+      </div>
       <ul v-if="hasCoaches">
         <coach-item
           v-for="coach in coaches"
@@ -15,7 +19,7 @@
         >
       </ul>
       <h3 v-else>No coaches found</h3>
-    </section>
+    </base-card>
   </div>
 </template>
 
@@ -35,5 +39,9 @@ ul {
   list-style: none;
   margin: 0;
   padding: 0;
+}
+.controls {
+  display: flex;
+  justify-content: space-between;
 }
 </style>
